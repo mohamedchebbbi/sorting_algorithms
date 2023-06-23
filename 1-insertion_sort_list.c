21d7e8c6ffc50cd2253acd9eb8716961d7e146d8
mohamedchebbbi <mohamedchebbbi@gmail.com>
2023-06-23 05:57:15 -0700

    insertion sort

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,39 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - Sorts a doubly linked list of integers in ascending
+ *
+ * @list: Double pointer to the head of the list
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current, *temp;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+
+	current = (*list)->next;
+	while (current != NULL)
+	{
+		temp = current;
+		current = current->next;
+
+		while (temp->prev != NULL && temp->n < temp->prev->n)
+		{
+			temp->prev->next = temp->next;
+			if (temp->next != NULL)
+				temp->next->prev = temp->prev;
+
+			temp->next = temp->prev;
+			temp->prev = temp->next->prev;
+			temp->next->prev = temp;
+
+			if (temp->prev == NULL)
+				*list = temp;
+			else
+				temp->prev->next = temp;
+
+			print_list(*list);
+		}
+	}
+}
--- sort.h
+++ sort.h
@@ -6,7 +6,7 @@
 /**
  * struct listint_s - Doubly linked list node
  *
- * @n: Integer stored in the node 
+ * @n: Integer stored in the node
  * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
  */
@@ -21,5 +21,6 @@ typedef struct listint_s
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
 
 #endif /*SORT_H*/
